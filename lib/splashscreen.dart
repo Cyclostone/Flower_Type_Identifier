a4cfd97ffcff6a3df61e7811b9a7af9bdea4de67
Arpit Shrotriya <arpit.shrotriya5945@gmail.com>
2021-07-13 05:33:02 +0530

    App Working

---
+++ lib/splashscreen.dart
@@ -0,0 +1,33 @@
+import 'package:flower_identifier_app/home.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplash extends StatefulWidget {
+  const MySplash({Key? key}) : super(key: key);
+
+  @override
+  _MySplashState createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: const Homepage(),
+      title: const Text(
+        'Sakura',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 40,
+          color: Colors.pink,
+        ),
+      ),
+      image: Image.asset('assets/sakura_icon2.png'),
+      backgroundColor: Colors.white,
+      photoSize: 180,
+      loaderColor: Colors.red,
+    );
+  }
+}
